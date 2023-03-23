f240584d541e4190ce0fc16144d2f2e0ad49db9e
zealzel <zealzel@gmail.com>
2023-03-23 14:39:00 +0800

    Add simulations_articubot_one in setup_vm

---
+++ setup_vm/simulations_articubot_one/articubot.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+source ../../scripts/utils.sh
+
+ORIGINAL_IMAGE="ubuntu-20.04.4-desktop-amd64.iso"
+IMAGE_DOWNLOAD_SITE=
+WORKSPACE="${1:-colcon_ws}"
+ROS_DISTRO="${ROS_DISTRO-galactic}"
+
+echo
+echo ====================================================================
+echo Basic check
+echo ====================================================================
+vcs_source=articubot.repos
+if [ -d ~/$WORKSPACE ]; then
+  cp $vcs_source ~/"$WORKSPACE" > /dev/null 2>&1
+else
+  echo "ERROR: $WORKSPACE does not exist"
+  exit 1
+fi
+
+echo "ROS_DISTRO: $ROS_DISTRO"
+echo "WORKSPACE: $WORKSPACE"
+echo "vcs_source: $vcs_source"
+
+echo
+echo ====================================================================
+echo Install ROS packages for ROS_DISTRO $ROS_DISTRO
+echo ====================================================================
+../../ros2/scripts/install_ros2_packages.sh $ROS_DISTRO
+
+echo
+echo ====================================================================
+echo Build tiago from source
+echo ====================================================================
+# customed repos file
+cd ~/$WORKSPACE
+vcs import src < $vcs_source
+rosdep install --from-paths src --ignore-src -r -y
+colcon build --symlink-install
+
+echo
+echo ====================================================================
+echo Append bashrc file
+echo ====================================================================
+append_bashrc "source /opt/ros/${ROS_DISTRO}/setup.bash"
+
+append_bashrc "export LIBGL_ALWAYS_SOFTWARE=1"
+append_bashrc "export OGRE_RTT_MODE=Copy"
+
+append_bashrc "ROS_DOMAIN_ID=0"
+append_bashrc "RMW_IMPLEMENTATION=rmw_cyclonedds_cpp"
+append_bashrc "CYCLONEDDS_URI=/etc/turtlebot4/cyclonedds_pc.xml"
