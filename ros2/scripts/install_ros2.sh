93555d3240a3d9253fa558461f31cb62d5df678f
zealzel <zealzel@gmail.com>
2023-03-23 09:02:25 +0800

    Create install_ros2.sh for all ROS2 distros

---
+++ ros2/scripts/install_ros2.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+
+# galactic: https://docs.ros.org/en/galactic/Installation/Ubuntu-Install-Debians.html
+# foxy: https://docs.ros.org/en/foxy/Installation/Ubuntu-Install-Debians.html
+# humble: https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debians.html
+
+# write a simple help message with examples
+echo "Usage: ./install_ros2.sh [UBUNTU_CODENAME] [ROS_DISTRO]"
+echo "default: UBUNTU_CODENAME=focal, ROS_DISTRO=galactic"
+
+source ../../scripts/utils.sh
+
+UBUNTU_CODENAME="${1-focal}"
+ROS_DISTRO="${2-galactic}" # galactic | foxy | humble
+ROS_INSTALL_TYPE="${3-desktop}" # desktop | ros-base
+
+ensure_sudo
+sudo apt-get update && sudo apt-get install -y curl gnupg
+
+echo =======================
+echo set locale
+echo =======================
+locale # check for UTF-8
+sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+locale # verify settings
+
+export DEBIAN_FRONTEND=noninteractive
+ln -fs /usr/share/zoneinfo/Asia/Taipei /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
+
+echo =============================
+echo Setup Sources
+echo =============================
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+echo =============================
+echo Install ROS 2 packages
+echo =============================
+sudo apt update
+sudo apt upgrade
+sudo apt install -y ros-${ROS_DISTRO}-${ROS_INSTALL_TYPE}
+sudo apt install -y ros-dev-tools
+
+echo
+echo ====================================================================
+echo Sourcing the setup script
+echo ====================================================================
+append_bashrc source /opt/ros/${ROS_DISTRO}/setup.bash
