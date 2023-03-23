af084cd42b924db00b414b3c4ea1280184ece713
zealzel <zealzel@gmail.com>
2023-03-23 10:39:16 +0800

    english comments

--- scripts/argparse_getopt.sh
+++ scripts/argparse_getopt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 将命令行参数转换为短选项和长选项
+# Translate long options to short options
 OPTIONS=u:r:i:a
 LONGOPTIONS=UBUNTU_CODENAME:,ROSDISTRO:,ROS_INSTALL_TYPE:,APPEND_SOURCE_SCRIPT_TO_BASHRC
 
@@ -13,14 +13,14 @@ echo "./my_script.sh -u focal  (短選項, 空格)"
 echo "./my_script.sh -a (--APPEND_SOURCE_SCRIPT_TO_BASHRC)  (開關選項)"
 echo
 
-# 解析命令行参数
+# Parse command line arguments
 PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTIONS --name "$0" -- "$@")
 if [[ $? -ne 0 ]]; then
-  # 解析失败，输出错误信息并退出
+  # parse failed, output error message and exit
   exit 1
 fi
 
-# 将解析后的命令行参数存储到相应的变量中
+# Save the parsed options to an array
 eval set -- "$PARSED"
 while true; do
   case "$1" in
@@ -55,13 +55,13 @@ while true; do
   esac
 done
 
-# 设置默认参数值
+# Set default values
 UBUNTU_CODENAME="${UBUNTU_CODENAME:-focal}"
 ROSDISTRO="${ROSDISTRO:-galactic}"
 ROS_INSTALL_TYPE="${ROS_INSTALL_TYPE:-desktop}"
 APPEND_SOURCE_SCRIPT_TO_BASHRC="${APPEND_SOURCE_SCRIPT_TO_BASHRC:-false}"
 
-# 输出参数值，用于测试
+# print the parsed options
 echo "UBUNTU_CODENAME=$UBUNTU_CODENAME"
 echo "ROSDISTRO=$ROSDISTRO"
 echo "ROS_INSTALL_TYPE=$ROS_INSTALL_TYPE"
