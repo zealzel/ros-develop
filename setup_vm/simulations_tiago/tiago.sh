5e1297e7368005c23c7a69c17c19702b38eda1e3
zealzel <zealzel@gmail.com>
2023-03-23 13:54:48 +0800

    Modify tiago.sh

--- setup_vm/simulations_tiago/tiago.sh
+++ setup_vm/simulations_tiago/tiago.sh
@@ -4,8 +4,8 @@ source ../../scripts/utils.sh
 
 ORIGINAL_IMAGE="ubuntu-20.04.4-desktop-amd64.iso"
 IMAGE_DOWNLOAD_SITE=
-ROS_DISTRO="galactic"
 WORKSPACE="${1:-colcon_ws}"
+ROS_DISTRO="${ROS_DISTRO-galactic}"
 
 echo
 echo ====================================================================
@@ -13,26 +13,27 @@ echo Build tiago from source
 echo ====================================================================
 # rosinstall/repos files are modified from https://github.com/pal-robotics/tiago_tutorials
 
+if [ "$ROS_DISTRO" = "humble" ]; then
+  vcs_source=tiago_public-"$ROS_DISTRO".repos
+else
+  vcs_source=tiago_public-"$ROS_DISTRO".rosinstall
+fi
+
 if [ -d ~/$WORKSPACE ]; then
-  cp tiago_public-"$ROS_DISTRO".rosinstall ~/"$WORKSPACE" > /dev/null 2>&1
-  cp tiago_public-"$ROS_DISTRO".repos ~/"$WORKSPACE" > /dev/null 2>&1
+  cp $vcs_source ~/"$WORKSPACE" > /dev/null 2>&1
   cd ~/$WORKSPACE
 else
   echo "ERROR: $WORKSPACE does not exist"
   exit 1
 fi
-if [ "$ROS_DISTRO" = "humble" ]; then
-  vcs import src < tiago_public-"$ROS_DISTRO".repos
-else
-  vcs import src < tiago_public-"$ROS_DISTRO".rosinstall
-fi
+vcs import src < $vcs_source
 colcon build --symlink-install
 
 echo
 echo ====================================================================
 echo Append bashrc file
 echo ====================================================================
-append_bashrc 'source /opt/ros/galactic/setup.bash'
+append_bashrc 'source /opt/ros/${ROS_DISTRO}/setup.bash'
 
 append_bashrc 'export LIBGL_ALWAYS_SOFTWARE=1'
 append_bashrc 'export OGRE_RTT_MODE=Copy'
