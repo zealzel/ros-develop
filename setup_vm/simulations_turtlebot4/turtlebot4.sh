151430c45b8eab02e1be7a82a72f4a2baa9de7b7
zealzel <zealzel@gmail.com>
2023-03-23 22:47:02 +0800

    Update turtlebot4.sh & add setup_vm/simulations_turtlebot4/tb4-{foxy,galactic}.repos

--- setup_vm/simulations_turtlebot4/turtlebot4.sh
+++ setup_vm/simulations_turtlebot4/turtlebot4.sh
@@ -2,8 +2,34 @@
 # source ../../scripts/utils.sh
 source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../../scripts/utils.sh"
 
-ROSDISTRO="galactic"
 WORKSPACE="colcon_ws"
+
+if [ "$ROS_DISTRO" = "" ]; then
+  echo "ERROR: ROSDISTRO is not set"
+  exit 1
+fi
+
+echo
+echo ====================================================================
+echo Basic check
+echo ====================================================================
+vcs_source=tb4-"$ROS_DISTRO".repos
+
+if [ -d ~/"$WORKSPACE" ]; then
+  cp "$vcs_source" ~/"$WORKSPACE" > /dev/null 2>&1
+else
+  echo "ERROR: $WORKSPACE does not exist"
+  exit 1
+fi
+
+echo
+echo ====================================================================
+echo Install ignition gazebo
+echo ====================================================================
+sudo apt-get install wget
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt-get update && sudo apt-get install ignition-edifice
 
 echo
 echo ====================================================================
@@ -26,26 +52,20 @@ ros_packages=(
 )
 ../../scripts/install_ros_packages.sh "$ROSDISTRO" "${ros_packages[@]}"
 
-# echo
-# echo ====================================================================
-# echo Build additional ROS packages from source
-# echo ====================================================================
-# source1="https://github.com/ros/ros_tutorials"
-# package1_location="~/$WORKSPACE/src/ros_tutorials"
-# git clone "$source1" -b "$ROSDISTRO-devel" "$package1_location"
-# cd "~/$WORKSPACE"
-# # find $package1_location -type f -name "COLCON_IGNORE" -exec rm {} \;
-# colcon build --symlink-install
+echo
+echo ====================================================================
+echo Build turtlebot4 from source
+echo ====================================================================
+# rosinstall/repos files are modified from https://github.com/pal-robotics/tiago_tutorials
+cd ~/"$WORKSPACE"
+vcs import src < "$vcs_source"
+rosdep install --from-paths src --ignore-src -r -y
+colcon build --symlink-install
 
-# echo
-# echo ====================================================================
-# echo Append bashrc file
-# echo ====================================================================
-# append_bashrc 'source /opt/ros/galactic/setup.bash'
-
-# append_bashrc 'export LIBGL_ALWAYS_SOFTWARE=1'
-# append_bashrc 'export OGRE_RTT_MODE=Copy'
-
-# append_bashrc 'ROS_DOMAIN_ID=0'
-# append_bashrc 'RMW_IMPLEMENTATION=rmw_cyclonedds_cpp'
-# append_bashrc 'CYCLONEDDS_URI=/etc/turtlebot4/cyclonedds_pc.xml'
+echo
+echo ====================================================================
+echo Append bashrc file
+echo ====================================================================
+append_bashrc 'source /opt/ros/galactic/setup.bash'
+append_bashrc 'export LIBGL_ALWAYS_SOFTWARE=1'
+append_bashrc 'export OGRE_RTT_MODE=Copy'
