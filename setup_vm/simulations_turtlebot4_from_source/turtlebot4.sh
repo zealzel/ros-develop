186650721808ae9ddde3fd5eb32344018cba0ad4
zealzel <zealzel@gmail.com>
2023-03-24 09:26:25 +0800

    Update turtlebot4.sh

--- setup_vm/simulations_turtlebot4_from_source/turtlebot4.sh
+++ setup_vm/simulations_turtlebot4_from_source/turtlebot4.sh
@@ -31,41 +31,20 @@ sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `ls
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 sudo apt-get update && sudo apt-get install ignition-edifice -y
 
-# echo
-# echo ====================================================================
-# echo Install turtlebot4 ros2 packages
-# echo ====================================================================
-# ros_packages=(
-#   "turtlebot4-desktop"
-#   "turtlebot4-base"
-#   "turtlebot4-bringup"
-#   "turtlebot4-description"
-#   "turtlebot4-diagnostics"
-#   "turtlebot4-msgs"
-#   "turtlebot4-navigation"
-#   "turtlebot4-node"
-#   "turtlebot4-robot"
-#   "turtlebot4-tests"
-#   "turtlebot4-simulator"
-#   "irobot-create-nodes"
-# )
-# ../../scripts/install_ros_packages.sh "$ROS_DISTRO" "${ros_packages[@]}"
-
 echo
 echo ====================================================================
 echo Install ROS packages for ROS_DISTRO $ROS_DISTRO
 echo ====================================================================
 ../../ros2/scripts/install_ros2_packages.sh $ROS_DISTRO
 
-# echo
-# echo ====================================================================
-# echo Build turtlebot4 from source
-# echo ====================================================================
-# # rosinstall/repos files are modified from https://github.com/pal-robotics/tiago_tutorials
-# cd ~/"$WORKSPACE"
-# vcs import src < "$vcs_source"
-# rosdep install --from-paths src --ignore-src -r -y
-# colcon build --symlink-install
+echo
+echo ====================================================================
+echo Build turtlebot4 from source
+echo ====================================================================
+cd ~/"$WORKSPACE"
+vcs import src < "$vcs_source"
+rosdep install --from-paths src --ignore-src -r -y
+colcon build --symlink-install
 
 echo
 echo ====================================================================
