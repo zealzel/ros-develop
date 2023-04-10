17e21d25514d67509aee560105b2f7f3e63fead4
zealzel <zealzel@gmail.com>
2023-04-10 17:14:19 +0800

    Add world_aws_robotmaker

--- setup_vm/setup_simulations.sh
+++ setup_vm/setup_simulations.sh
@@ -4,5 +4,10 @@ source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../scripts/utils.sh"
 WORKSPACE="${1:-simulations}"
 ROS_DISTRO="${ROS_DISTRO-galactic}"
 
+./simulations_turtlebot3/turtlebot3.sh
+
 ./simulations_articubot_one/articubot.sh $WORKSPACE
 ./simulations_tiago/tiago.sh $WORKSPACE
+
+rm -f $WORKSPACE/*.repos
+rm -f $WORKSPACE/*.rosinstall
