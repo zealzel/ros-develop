2a79e66d11d3b2d8a2504809ff0514da4ceaa6c0
zealzel <zealzel@gmail.com>
2023-04-11 09:20:28 +0800

    Add install from source script.sh for genereal use

---
+++ setup_vm/ros2_append_bashrc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo
+echo ====================================================================
+echo Append bashrc file
+echo ====================================================================
+append_bashrc "source /opt/ros/${ROS_DISTRO}/setup.bash"
+
+append_bashrc "export LIBGL_ALWAYS_SOFTWARE=1"
+append_bashrc "export OGRE_RTT_MODE=Copy"
+
+append_bashrc "ROS_DOMAIN_ID=0"
+append_bashrc "RMW_IMPLEMENTATION=rmw_cyclonedds_cpp"
+append_bashrc "CYCLONEDDS_URI=/etc/turtlebot4/cyclonedds_pc.xml"
--- setup_vm/setup_simulations.sh
+++ setup_vm/setup_simulations.sh
@@ -4,10 +4,25 @@ source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../scripts/utils.sh"
 WORKSPACE="${1:-simulations}"
 ROS_DISTRO="${ROS_DISTRO-galactic}"
 
-./simulations_turtlebot3/turtlebot3.sh
+echo "==============================================="
+echo "Install robots from package manager"
+echo "==============================================="
+# ./simulations_turtlebot3/turtlebot3.sh
 
-./simulations_articubot_one/articubot.sh $WORKSPACE
+echo "==============================================="
+echo "Build/Install robots from source"
+echo "==============================================="
+./install_from_source.sh $WORKSPACE "false" "simulations_articubot_one/articubot.repos"
+# ./simulations_articubot_one/articubot.sh $WORKSPACE
+./install_from_source.sh $WORKSPACE "false" "simulations_neobotix/neobotix.repos"
+
+# exceptions not using install_from_source.sh
 ./simulations_tiago/tiago.sh $WORKSPACE
+
+echo "==============================================="
+echo "append bashrc for ROS2"
+echo "==============================================="
+./ros2_append_bashrc.sh
 
 rm -f $WORKSPACE/*.repos
 rm -f $WORKSPACE/*.rosinstall
