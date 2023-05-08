2b7158e760687410ac9549e21ba134bc78a129f8
zealzel <zealzel@gmail.com>
2023-05-08 14:18:06 +0800

    update

---
+++ setup_raspberry/create_workspace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+workspace=$1
+if [ ! -d "$HOME/$workspace" ]; then
+    echo "目錄 ~/$workspace 不存在，是否要建立 ~/$workspace/src? (y/n)"
+    read answer
+
+    if [ "$answer" == "y" ]; then
+        mkdir -p "$HOME/$workspace/src"
+        echo "已成功建立 ~/$workspace/src"
+    else
+        exit 1
+    fi
+else
+    echo "~/$workspace 目錄已存在"
+fi
--- setup_raspberry/zbot1.5-articubot/zbot1.5_articubot.sh
+++ setup_raspberry/zbot1.5-articubot/zbot1.5_articubot.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../../scripts/utils.sh"
 
+WORKSPACE="${1:-zbot_ws}"
+ROS_DISTRO="${ROS_DISTRO-galactic}"
+./create_workspace.sh $WORKSPACE || exit_code=$?
+if [[ $exit_code -ne 0 ]]; then
+  exit
+fi
+
 ORIGINAL_IMAGE="[488d07354c8b92592c3c0e759b0f4730dce21dce]ubuntu-20.04.5-preinstalled-server-arm64+raspi.img.xz"
 IMAGE_DOWNLOAD_SITE=
-
 echo "The original image: $ORIGINAL_IMAGE"
 echo "The original image donwload site: $IMAGE_DOWNLOAD_SITE"
 
@@ -11,20 +17,20 @@ echo
 echo ====================================================================
 echo Install ROS2
 echo ====================================================================
-../ros2/scripts/install_ros2.sh
-../ros2/scripts/install_ros2_packages.sh
+../../ros2/scripts/install_ros2.sh
+../../ros2/scripts/install_ros2_packages.sh
 
 echo
 echo ====================================================================
 echo Create udev rules
 echo ====================================================================
-../scripts/create_zbot1.5_udev_files.sh
+../../scripts/create_zbot1.5_udev_files.sh
 
 echo
 echo ===============================================
 echo Build/Install robots packages from source
 echo ===============================================
-./install_from_source.sh $WORKSPACE "false" "zbot1.5_articubot.repos"
+../install_from_source.sh $WORKSPACE "false" "zbot1.5_articubot.repos"
 
 echo
 echo ====================================================================
