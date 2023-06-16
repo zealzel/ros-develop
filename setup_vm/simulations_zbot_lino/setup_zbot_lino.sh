b5a9c77f8d6e587d1eb895bba54d60e93d9ec501
zealzel <zealzel@gmail.com>
2023-06-16 23:30:09 +0800

    Add setup_zbot_lino.sh

---
+++ setup_vm/simulations_zbot_lino/setup_zbot_lino.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+WORKSPACE="${1:-simulations}"
+
+if [[ -d "$HOME/$WORKSPACE" ]]; then
+  echo "$HOME/$WORKSPACE exist"
+else
+  echo "$HOME/$WORKSPACE does not exist"
+  exit
+fi
+
+TARGET_SCRIPT_ABSOLUTE_PATH="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/../install_from_source.sh")"
+"${TARGET_SCRIPT_ABSOLUTE_PATH}" $WORKSPACE "false" "simulations_zbot_lino/zbot_lino.repos"
