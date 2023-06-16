d72050b91eebbdfdd498da5eb09957ea2538ad7a
zealzel <zealzel@gmail.com>
2023-06-16 16:24:38 +0800

    Add install_ros_common_packages.sh

---
+++ scripts/install_ros_common_packages.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# source ../../scripts/utils.sh
+source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/utils.sh"
+
+ROS1DISTRO_ARRY=("melodic" "noetic")
+ROS2DISTRO_ARRY=("galactic" "foxy" "humble")
+
+if [[ " ${ROS1DISTRO_ARRY[@]} " =~ " ${ROS_DISTRO} " ]]; then
+  echo "ROS1 distro: $ROSDISTRO currently is not supported"
+  exit 1
+elif [[ " ${ROS2DISTRO_ARRY[@]} " =~ " ${ROSDISTRO} " ]]; then
+else
+  echo "ROS distro: $ROSDISTRO is not supported"
+  exit 1
+fi
+
+ros_packages=(
+  # basic
+  "ros-$ROS_DISTRO-xacro"
+  "ros-$ROS_DISTRO-joy-teleop"
+  # tools
+  "ros-$ROS_DISTRO-joint-state-publisher-gui"
+  "ros-$ROS_DISTRO-twist-mux"
+  # rqt
+  "ros-$ROS_DISTRO-rqt-tf-tree"
+  # gazebo
+  "ros-$ROS_DISTRO-gazebo-plugins"
+  # learning
+  "ros-$ROS_DISTRO-turtlesim"
+  # ros2_control stack
+  "ros-$ROS_DISTRO-ros2-control"
+  "ros-$ROS_DISTRO-ros2-controllers"
+  "ros-$ROS_DISTRO-control-toolbox"
+  "ros-$ROS_DISTRO-control-msgs"
+  "ros-$ROS_DISTRO-gazebo-ros2-control"
+  # slam/nav
+  "ros-$ROS_DISTRO-slam-toolbox"
+  "ros-$ROS_DISTRO-navigation2"
+  "ros-$ROS_DISTRO-nav2-bringup"
+  # robot
+  # "ros-$ROS_DISTRO-turtlebot3-simulation"
+)
+
+install_ubuntu_packages "${ros_packages[@]}"
