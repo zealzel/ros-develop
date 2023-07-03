0f679a9498f464413aeeaf5a776139a39f8cffe2
zealzel <zealzel@gmail.com>
2023-07-03 23:57:34 +0800

    Update install_gazebo.sh

--- scripts/install_gazebo.sh
+++ scripts/install_gazebo.sh
@@ -10,8 +10,10 @@ curl -sSL http://get.gazebosim.org | sh
 # Install gazebo_ros_pkgs
 # ref: http://classic.gazebosim.org/tutorials?tut=ros2_installing&cat=connect_ros
 if [[ " ${ROS1DISTRO_ARRY[@]} " =~ " ${ROS_DISTRO} " ]]; then
+  sudo apt install "ros-${ROS_DISTRO}-gazebo-ros-pkgs"
+elif [[ " ${ROS2DISTRO_ARRY[@]} " =~ " ${ROS_DISTRO} " ]]; then
+  sudo apt install "ros-${ROS_DISTRO}-gazebo-ros-pkgs"
+else
   echo "ROS1 distro: $ROS_DISTRO currently is not supported"
   exit 1
-elif [[ " ${ROS2DISTRO_ARRY[@]} " =~ " ${ROS_DISTRO} " ]]; then
-    sudo apt install "ros-${ROS_DISTRO}-gazebo-ros-pkgs"
 fi
