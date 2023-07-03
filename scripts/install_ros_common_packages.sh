1022abaf916c866a50ab47807ef26329089fdfe0
zealzel <zealzel@gmail.com>
2023-07-04 00:14:49 +0800

    Update install_ros_common_packages.sh

--- scripts/install_ros_common_packages.sh
+++ scripts/install_ros_common_packages.sh
@@ -6,8 +6,27 @@ ROS1DISTRO_ARRY=("melodic" "noetic")
 ROS2DISTRO_ARRY=("galactic" "foxy" "humble")
 
 if [[ " ${ROS1DISTRO_ARRY[@]} " =~ " ${ROS_DISTRO} " ]]; then
-  echo "ROS1 distro: $ROS_DISTRO currently is not supported"
-  exit 1
+  ros_packages=(
+    # basic
+    "ros-$ROS_DISTRO-xacro"
+    "ros-$ROS_DISTRO-teleop-twist-keyboard"
+    "ros-$ROS_DISTRO-joy-teleop"
+    # tools
+    # "ros-$ROS_DISTRO-joint-state-publisher-gui"
+    # "ros-$ROS_DISTRO-twist-mux"
+    # rqt
+    "ros-$ROS_DISTRO-rqt-tf-tree"
+    # gazebo
+    "ros-$ROS_DISTRO-gazebo-plugins"
+    # learning
+    "ros-$ROS_DISTRO-turtlesim"
+    # ros2_control stack
+    "ros-$ROS_DISTRO-ros-control"
+    "ros-$ROS_DISTRO-ros-controllers"
+    "ros-$ROS_DISTRO-control-toolbox"
+    "ros-$ROS_DISTRO-control-msgs"
+    "ros-$ROS_DISTRO-gazebo-ros-control"
+  )
 elif [[ " ${ROS2DISTRO_ARRY[@]} " =~ " ${ROS_DISTRO} " ]]; then
   ros_packages=(
     # basic
