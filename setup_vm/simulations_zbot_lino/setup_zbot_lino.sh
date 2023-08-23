480aa5c7047b85e0e28ef1841c4a08fa4f912e36
zealzel <zealzel@gmail.com>
2023-08-23 08:44:13 +0800

    Update setup_zbot_lino.sh

--- setup_vm/simulations_zbot_lino/setup_zbot_lino.sh
+++ setup_vm/simulations_zbot_lino/setup_zbot_lino.sh
@@ -8,12 +8,15 @@ if [[ -d "$HOME/$WORKSPACE" ]]; then
   echo "$HOME/$WORKSPACE exist"
 else
   echo "$HOME/$WORKSPACE does not exist"
-  exit
+  TARGET_SCRIPT_ABSOLUTE_PATH="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/../create_workspace.sh")"
+  $TARGET_SCRIPT_ABSOLUTE_PATH "$WORKSPACE" || exit_code=$?
+  if [[ $exit_code -ne 0 ]]; then
+    exit
+  fi
 fi
 
 TARGET_SCRIPT_ABSOLUTE_PATH="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/../install_from_source.sh")"
-"${TARGET_SCRIPT_ABSOLUTE_PATH}" $WORKSPACE "false" "simulations_zbot_lino/zbot_lino.repos"
-
+"$TARGET_SCRIPT_ABSOLUTE_PATH" "$WORKSPACE" "false" "simulations_zbot_lino/zbot_lino.repos"
 
 echo "export LINOROBOT2_BASE=$BASE" >> ~/.bashrc
 echo "export LINOROBOT2_LASER_SENSOR=$LASER_SENSOR" >> ~/.bashrc
