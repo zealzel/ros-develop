333a9cd0e16b9b177ae69e6e4a12254a18974598
zealzel <zealzel@gmail.com>
2023-08-29 17:00:50 +0800

    Modify install_ros2.sh and install_ros2_packages.sh to consider different ROSDISTRO

--- ros2/scripts/install_ros2_packages.sh
+++ ros2/scripts/install_ros2_packages.sh
@@ -2,31 +2,36 @@
 # source ../../scripts/utils.sh
 source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../../scripts/utils.sh"
 
-ROS_DISTRO="${1-galactic}"
+source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../../scripts/utils.sh"
+source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../../scripts/argparse_ros.sh"
+
+parse_args "$@"
+
+echo "ROSDISTRO=$ROSDISTRO"
 
 ros_packages=(
   # basic
-  "ros-$ROS_DISTRO-xacro"
-  "ros-$ROS_DISTRO-joy-teleop"
+  "ros-$ROSDISTRO-xacro"
+  "ros-$ROSDISTRO-joy-teleop"
   # tools
-  "ros-$ROS_DISTRO-joint-state-publisher-gui"
-  "ros-$ROS_DISTRO-twist-mux"
+  "ros-$ROSDISTRO-joint-state-publisher-gui"
+  "ros-$ROSDISTRO-twist-mux"
   # rqt
-  "ros-$ROS_DISTRO-rqt-tf-tree"
+  "ros-$ROSDISTRO-rqt-tf-tree"
   # gazebo
-  "ros-$ROS_DISTRO-gazebo-plugins"
+  "ros-$ROSDISTRO-gazebo-plugins"
   # learning
-  "ros-$ROS_DISTRO-turtlesim"
+  "ros-$ROSDISTRO-turtlesim"
   # ros2_control stack
-  "ros-$ROS_DISTRO-ros2-control"
-  "ros-$ROS_DISTRO-ros2-controllers"
-  "ros-$ROS_DISTRO-control-toolbox"
-  "ros-$ROS_DISTRO-control-msgs"
-  "ros-$ROS_DISTRO-gazebo-ros2-control"
+  "ros-$ROSDISTRO-ros2-control"
+  "ros-$ROSDISTRO-ros2-controllers"
+  "ros-$ROSDISTRO-control-toolbox"
+  "ros-$ROSDISTRO-control-msgs"
+  "ros-$ROSDISTRO-gazebo-ros2-control"
   # slam/nav
-  "ros-$ROS_DISTRO-slam-toolbox"
-  "ros-$ROS_DISTRO-navigation2"
-  "ros-$ROS_DISTRO-nav2-bringup"
+  "ros-$ROSDISTRO-slam-toolbox"
+  "ros-$ROSDISTRO-navigation2"
+  "ros-$ROSDISTRO-nav2-bringup"
   # robot
   # "ros-$ROS_DISTRO-turtlebot3-simulation"
 )
