295e9a169ccbb99157496ffca56578fb889992e7
zealzel <zealzel@gmail.com>
2023-08-30 22:46:15 +0800

    remove non-used code in zbot1.5_articubot_humble.sh

--- setup_raspberry/zbot1.5_articubot/zbot1.5_articubot_humble.sh
+++ setup_raspberry/zbot1.5_articubot/zbot1.5_articubot_humble.sh
@@ -53,10 +53,3 @@ echo ===============================================
 echo Build/Install robots packages from source
 echo ===============================================
 source "/opt/ros/${ROSDISTRO}/setup.bash" && ../../scripts/install_from_source.sh $WORKSPACE $ROSDISTRO "zbot1.5_articubot_humble.repos" "false"
-# ../../scripts/install_from_source.sh $WORKSPACE $ROSDISTRO "zbot1.5_articubot_humble.repos" "false"
-
-# echo
-# echo ====================================================================
-# echo Append bashrc file
-# echo ====================================================================
-# append_bashrc "source /opt/ros/$ROSDISTRO/setup.bash"
