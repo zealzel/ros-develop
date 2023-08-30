dc1c9fb502b8b55c8422ea8d344f8388a9eb3d13
zealzel <zealzel@gmail.com>
2023-08-30 23:05:04 +0800

    Add prepare_ros2_workspace.sh and zbot_artic_humble.sh

---
+++ ros2/scripts/prepare_ros2_workspace.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../scripts/utils.sh"
+source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../scripts/argparse_ros.sh"
+parse_args "$@"
+
+../scripts/create_workspace.sh $WORKSPACE || exit_code=$?
+if [[ $exit_code -ne 0 ]]; then
+  exit
+fi
+
+echo
+echo ====================================================================
+echo Install ROS2
+echo ====================================================================
+./install_ros2.sh -u $UBUNTU_CODENAME -r $ROSDISTRO
+./install_ros2_packages.sh -r $ROSDISTRO
+
+echo
+echo ====================================================================
+echo Ensure rosdep is initialized
+echo ====================================================================
+rosdep update || exit_code=$?
+if [[ $exit_code -ne 0 ]]; then
+  sudo rosdep init
+  rosdep update --include-eol-distros
+fi
+
+export UBUNTU_CODENAME=$UBUNTU_CODENAME
+export ROSDISTRO=$ROSDISTRO
+export WORKSPACE=$WORKSPACE
+export ROS_INSTALL_TYPE=$ROS_INSTALL_TYPE
---
+++ setup_raspberry/zbot1.5_articubot/zbot_artic_humble.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+UBUNTU_CODENAME="jammy"
+ROSDISTRO="humble"
+WORKSPACE="zbotartic_ws"
+echo "UBUNTU_CODENAME=$UBUNTU_CODENAME"
+echo "ROSDISTRO=$ROSDISTRO"
+echo "WORKSPACE=$WORKSPACE"
+../../ros2/scripts/prepare_ros2_workspace.sh -u $UBUNTU_CODENAME -r $ROSDISTRO -w $WORKSPACE
+../../scripts/create_workspace.sh $WORKSPACE || exit_code=$?
+if [[ $exit_code -ne 0 ]]; then
+  exit
+fi
+
+ORIGINAL_IMAGE="[1ebe853ca69ce507a69f97bb70f13bc1ffcfa7a2]ubuntu-22.04.2-preinstalled-server-arm64+raspi.img.xz"
+IMAGE_DOWNLOAD_SITE=
+echo "The original image: $ORIGINAL_IMAGE"
+echo "The original image donwload site: $IMAGE_DOWNLOAD_SITE"
+
+echo
+echo ====================================================================
+echo Create udev rules
+echo ====================================================================
+./create_udev.sh
+
+echo ===============================================
+echo Install robots from package manager
+echo ===============================================
+../../scripts/install_from_apt.sh $WORKSPACE "humble" "ros_packages.sh" "false"
+
+echo
+echo ===============================================
+echo Build/Install robots packages from source
+echo ===============================================
+source "/opt/ros/${ROSDISTRO}/setup.bash" && ../../scripts/install_from_source.sh $WORKSPACE $ROSDISTRO "zbot1.5_articubot_humble.repos" "false"
