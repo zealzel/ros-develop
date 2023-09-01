bec157d2646f580a4786bd54f08428e9f0924c14
zealzel <zealzel@gmail.com>
2023-09-01 12:37:05 +0800

    Add zbot_lino.sh. Support both focal/galactic & jammy/humble.

--- setup_raspberry/zbot_lino/zbot_lino.sh
+++ setup_raspberry/zbot_lino/zbot_lino.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
-UBUNTU_CODENAME="jammy"
-ROSDISTRO="humble"
+UBUNTU_CODENAME=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d"=" -f2)
+
+if [[ "$UBUNTU_CODENAME" == "focal" ]]; then
+  echo "Ubuntu 20.04 detected. Set ROSDISTRO to galactic."
+  ROSDISTRO="galactic"
+  ORIGINAL_IMAGE="[488d07354c8b92592c3c0e759b0f4730dce21dce]ubuntu-20.04.5-preinstalled-server-arm64+raspi.img.xz"
+  IMAGE_DOWNLOAD_SITE=
+elif [[ "$UBUNTU_CODENAME" == "jammy" ]]; then
+  echo "Ubuntu 22.04 detected. Set ROSDISTRO to humble."
+  ROSDISTRO="humble"
+  ORIGINAL_IMAGE="[1ebe853ca69ce507a69f97bb70f13bc1ffcfa7a2]ubuntu-22.04.2-preinstalled-server-arm64+raspi.img.xz"
+  IMAGE_DOWNLOAD_SITE=
+else
+  echo "Ubuntu $UBUNTU_CODENAME is not supported"
+  exit 1
+fi
+
+echo "The original image: $ORIGINAL_IMAGE"
+echo "The original image download site: $IMAGE_DOWNLOAD_SITE"
+
 WORKSPACE="zbotlino_ws"
 echo "UBUNTU_CODENAME=$UBUNTU_CODENAME"
 echo "ROSDISTRO=$ROSDISTRO"
 echo "WORKSPACE=$WORKSPACE"
 ../../ros2/scripts/prepare_ros2_workspace.sh -u "$UBUNTU_CODENAME" -r "$ROSDISTRO" -w "$WORKSPACE"
-
-ORIGINAL_IMAGE="[1ebe853ca69ce507a69f97bb70f13bc1ffcfa7a2]ubuntu-22.04.2-preinstalled-server-arm64+raspi.img.xz"
-IMAGE_DOWNLOAD_SITE=
-echo "The original image: $ORIGINAL_IMAGE"
-echo "The original image donwload site: $IMAGE_DOWNLOAD_SITE"
 
 source /opt/ros/"$ROSDISTRO"/setup.bash
 ROS_DISTRO="$(printenv ROS_DISTRO)"
