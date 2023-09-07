f80c4f94c6debe1dba938117cf182a3213d9cd06
zealzel <zealzel@gmail.com>
2023-09-07 17:25:27 +0800

    Update ros2_append_bashrc.sh

--- setup_vm/ros2_append_bashrc.sh
+++ setup_vm/ros2_append_bashrc.sh
@@ -8,14 +8,13 @@ append_bashrc "source /usr/share/gazebo/setup.bash"
 append_bashrc "export LIBGL_ALWAYS_SOFTWARE=1"
 append_bashrc "export OGRE_RTT_MODE=Copy"
 append_bashrc "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp"
-append_bashrc "export ROS_DOMAIN_ID=0"
+# append_bashrc "export ROS_DOMAIN_ID=0"
 # append_bashrc "export CYCLONEDDS_URI=/etc/turtlebot4/cyclonedds_pc.xml"
 
 source /opt/ros/"$ROS_DISTRO"/setup.bash
 source /usr/share/gazebo/setup.bash
 export LIBGL_ALWAYS_SOFTWARE=1
 export OGRE_RTT_MODE=Copy
-export ROS_DOMAIN_ID=0
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 # export ROS_DOMAIN_ID=0
 #export CYCLONEDDS_URI=/etc/turtlebot4/cyclonedds_pc.xml
