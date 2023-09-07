47270ece560da844256e452110f03f1ed77bc307
zealzel <zealzel@gmail.com>
2023-09-07 12:14:22 +0800

    Update setup_template.sh

--- setup_vm/setup_template.sh
+++ setup_vm/setup_template.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
+source "$script_dir/../scripts/utils.sh"
+source "$script_dir/../scripts/argparse_ros.sh"
 script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 install_from_source_sh="$(readlink -f $script_dir/../scripts/install_from_source.sh)"
 install_from_apt_sh="$(readlink -f $script_dir/../scripts/install_from_apt.sh)"
-source "$script_dir/../scripts/utils.sh"
-source "$script_dir/../scripts/argparse_ros.sh"
+
 WORKSPACE="simulations"
 UBUNTU_CODENAME=$(cat /etc/os-release |grep VERSION_CODENAME|cut -d"=" -f2)
+if [[ "$UBUNTU_CODENAME" == "focal" ]]; then
+  echo "Ubuntu 20.04 detected. Set defualt ROSDISTRO to galactic."
+  ROSDISTRO="galactic"
+elif [[ "$UBUNTU_CODENAME" == "jammy" ]]; then
+  echo "Ubuntu 22.04 detected. Set defualt ROSDISTRO to humble."
+  ROSDISTRO="humble"
+else
+  echo "Ubuntu $UBUNTU_CODENAME is not supported"
+  exit 1
+fi
+
 parse_args "$@"
+echo "UBUNTU_CODENAME=$UBUNTU_CODENAME"
+echo "ROSDISTRO=$ROSDISTRO"
+echo "WORKSPACE=$WORKSPACE"
+
+echo ===============================================
+echo Prepare workspace for ROS2 development
+echo ===============================================
+../ros2/scripts/prepare_ros2_workspace.sh -u $UBUNTU_CODENAME -r $ROSDISTRO -w $WORKSPACE
+
+ROS_DISTRO="$(printenv ROS_DISTRO)"
 source /opt/ros/${ROSDISTRO}/setup.bash >/dev/null 2>&1 || exit_code=$?
 if [[ $exit_code -ne 0 ]]; then
   echo "/opt/ros/$ROSDISTRO/setup.sh does not exist."
   print_usage
   exit
 fi
-ROS_DISTRO="$(printenv ROS_DISTRO)"
-../ros2/scripts/prepare_ros2_workspace.sh -u $UBUNTU_CODENAME -r $ROS_DISTRO -w $WORKSPACE
 
 # Added temporarily for testing
 ../ros2/scripts/install_mppi_controllers.sh -r $ROS_DISTRO -w $WORKSPACE
@@ -44,4 +64,3 @@ echo ===============================================
 
 rm -f $WORKSPACE/*.repos
 rm -f $WORKSPACE/*.rosinstall
-
