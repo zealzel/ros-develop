209bd5a71078e7a07f5e3ab7cec934dfeb35f445
zealzel <zealzel@gmail.com>
2023-09-22 12:35:46 +0800

    Update install_rmf.sh

--- setup_vm/install_rmf.sh
+++ setup_vm/install_rmf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-install_from_source_sh="$(readlink -f $script_dir/../scripts/install_from_source.sh)"
-install_from_apt_sh="$(readlink -f $script_dir/../scripts/install_from_apt.sh)"
+install_from_source_sh="$(readlink -f "$script_dir"/../scripts/install_from_source.sh)"
+install_from_apt_sh="$(readlink -f "$script_dir"/../scripts/install_from_apt.sh)"
 source "$script_dir/../scripts/utils.sh"
 source "$script_dir/../scripts/argparse_ros.sh"
 
 WORKSPACE="simulations"
-UBUNTU_CODENAME=$(cat /etc/os-release |grep VERSION_CODENAME|cut -d"=" -f2)
+UBUNTU_CODENAME=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d"=" -f2)
 if [[ "$UBUNTU_CODENAME" == "focal" ]]; then
   ROSDISTRO="galactic"
 elif [[ "$UBUNTU_CODENAME" == "jammy" ]]; then
@@ -36,8 +36,12 @@ echo Install from package manager
 echo ===============================================
 install_ubuntu_packages "${ros_packages[@]}"
 
-
 echo ===============================================
 echo Build/Install from source
 echo ===============================================
-"$install_from_source_sh" $WORKSPACE "$script_dir/rmf/rmf_$ROS_DISTRO.repos"
+# "$install_from_source_sh" "$WORKSPACE" "$script_dir/rmf/rmf_$ROS_DISTRO.repos"
+
+if [[ "$ROSDISTRO" == "humble" ]]; then
+  # temporary ignore ignition related packages since they will fail during building
+  "$install_from_source_sh" "$WORKSPACE" "$script_dir/rmf/rmf_$ROS_DISTRO.repos" -i rmf_building_sim_gz_plugins,rmf_robot_sim_gz_plugins
+fi
