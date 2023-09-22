3044d95fb50a533ed97392029e631ff60ea324fc
zealzel <zealzel@gmail.com>
2023-09-22 12:07:28 +0800

    Update the install_from_source.sh. Add arguments -i/--IGNORES to ignore some packages. These will add COLCON_IGNORE files to the packages.

--- scripts/install_from_source.sh
+++ scripts/install_from_source.sh
@@ -7,7 +7,7 @@ source "$script_dir/../scripts/argparse.sh"
 declare -A arg_desc=(
   ["-w,--WORKSPACE"]="Workspace name (default: colcon_ws)"
   ["-v,--VCS_REPOS"]="vcs repos"
-  ["-x,--EXCLUDES"]="excluded ros packages"
+  ["-i,--IGNORES"]="ignored packages which are separated by comma. COLCON_IGNORE will be added to the package"
 )
 
 declare -A parsed_args
@@ -15,15 +15,15 @@ parse_args "$@"
 WORKSPACE=${parsed_args["WORKSPACE"]-ros2_ws}
 VCS_REPOS=${parsed_args["VCS_REPOS"]}
 VCS_REPOS=$(readlink -f $VCS_REPOS)
-EXCLUDES=${parsed_args["EXCLUDES"]}
-IFS=',' read -ra EXCLUDES_ARRAY <<< "$EXCLUDES"
+IGNORES=${parsed_args["IGNORES"]}
+IFS=',' read -ra ignored_ros_packages <<< "$IGNORES"
 
 echo "WORKSPACE: $WORKSPACE"
 echo "VCS_REPOS: $VCS_REPOS"
-if [ -n "$EXCLUDES" ]; then
-    echo "EXCLUDES: "
-    for ex in "${EXCLUDES_ARRAY[@]}"; do
-      echo "$ex"
+if [ -n "$IGNORES" ]; then
+    echo "IGNORES: "
+    for item in "${ignored_ros_packages[@]}"; do
+      echo "$item"
     done
 fi
 
@@ -45,18 +45,12 @@ echo ====================================================================
 cd $HOME/"$WORKSPACE"
 vcs import src < "$VCS_REPOS"
 
-
-# ignored_ros_packages=(
-#   "ros-$ROSDISTRO-xacro"
-#   "ros-$ROSDISTRO-joy-teleop"
-# )
-
-# for pkg in "${ignored_ros_packages[@]}"; do
-#     pkg_path=$(find src -type d -name $pkg)
-#     echo $pkg_path
-# done
-
-# find src -type d -name rmf_building_sim_gz_classic_plugins
+for pkg in "${ignored_ros_packages[@]}"; do
+    pkg_path=$(find src -type d -name $pkg)
+    echo "$pkg --> $pkg_path"
+    cd "$WORKSPACEPATH/$pkg_path" && touch COLCON_IGNORE
+    cd "$WORKSPACEPATH"
+done
 
 rosdep install --from-paths src --ignore-src -r -y
 colcon build --symlink-install
