c1fc759dad6707e49fa6a1b02531b6962eb1e4bd
zealzel <zealzel@gmail.com>
2023-09-25 15:42:00 +0800

    Update setup_zbot_lino.sh

--- setup_vm/simulations_zbot_lino/setup_zbot_lino.sh
+++ setup_vm/simulations_zbot_lino/setup_zbot_lino.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
-WORKSPACE="${1:-simulations}"
-BASE=2wd
-LASER_SENSOR=rplidar
-DEPTH_SENSOR=realsense
+script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+install_from_source_sh="$(readlink -f $script_dir/../../scripts/install_from_source.sh)"
+source "$script_dir/../../scripts/argparse_ros.sh"
 
-if [[ -d "$HOME/$WORKSPACE" ]]; then
-  echo "$HOME/$WORKSPACE exist"
+WORKSPACE="simulations"
+UBUNTU_CODENAME=$(cat /etc/os-release |grep VERSION_CODENAME|cut -d"=" -f2)
+if [[ "$UBUNTU_CODENAME" == "focal" ]]; then
+  echo "Ubuntu 20.04 detected. Set defualt ROSDISTRO to galactic."
+  ROSDISTRO="galactic"
+elif [[ "$UBUNTU_CODENAME" == "jammy" ]]; then
+  echo "Ubuntu 22.04 detected. Set defualt ROSDISTRO to humble."
+  ROSDISTRO="humble"
 else
-  echo "$HOME/$WORKSPACE does not exist"
+  echo "Ubuntu $UBUNTU_CODENAME is not supported"
+  exit 1
+fi
+
+parse_args "$@"
+echo "UBUNTU_CODENAME=$UBUNTU_CODENAME"
+echo "ROSDISTRO=$ROSDISTRO"
+echo "WORKSPACE=$WORKSPACE"
+
+source /opt/ros/${ROSDISTRO}/setup.bash >/dev/null 2>&1 || exit_code=$?
+if [[ $exit_code -ne 0 ]]; then
+  echo "/opt/ros/$ROSDISTRO/setup.sh does not exist."
+  print_usage
   exit
 fi
 
-TARGET_SCRIPT_ABSOLUTE_PATH="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/../install_from_source.sh")"
-"$TARGET_SCRIPT_ABSOLUTE_PATH" "$WORKSPACE" "false" "simulations_zbot_lino/zbot_lino.repos"
-
-export LINOROBOT2_BASE=$BASE
-export LINOROBOT2_LASER_SENSOR=$LASER_SENSOR
-export LINOROBOT2_DEPTH_SENSOR=$DEPTH_SENSOR
-echo "export LINOROBOT2_BASE=$BASE" >> ~/.bashrc
-echo "export LINOROBOT2_LASER_SENSOR=$LASER_SENSOR" >> ~/.bashrc
-echo "export LINOROBOT2_DEPTH_SENSOR=$DEPTH_SENSOR" >> ~/.bashrc
+ROS_DISTRO="$(printenv ROS_DISTRO)"
+"$install_from_source_sh" -w $WORKSPACE -v "zbot_lino_$ROS_DISTRO.repos"
