ee528bb66f3e5c7a5035f08477151a85525c7e74
zealzel <zealzel@gmail.com>
2023-10-24 15:52:39 +0800

    Update setup_network.sh & add cyclonedds.xml

--- setup_raspberry/zbot_lino/set_network.sh
+++ setup_raspberry/zbot_lino/set_network.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+source "$script_dir/../../scripts/utils.sh"
 
+# copy netplan config file
 filepath="/etc/netplan/50-cloud-init.yaml"
 sudo cp "$filepath" "$filepath.bak"
 sudo cp 50-cloud-init.yaml "$filepath"
+
+# copy cyclonedds config file
+cp cyclonedds.xml $HOME
+append_bashrc 'export CYCLONEDDS_URI=/path/to/cyclonedds_config.xml'
