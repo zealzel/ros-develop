c51253398b9191dd0fc6a3f680b630b23f137679
zealzel <zealzel@gmail.com>
2023-11-19 16:11:44 +0800

    Add more description to the install_mppi_controllers.sh

--- ros2/scripts/install_mppi_controllers.sh
+++ ros2/scripts/install_mppi_controllers.sh
@@ -18,7 +18,7 @@ fi
 parse_args "$@"
 
 # LATEST_WORKED_COMMIT="1.1.12"
-LATEST_WORKED_COMMIT="3ed4c2df"
+LATEST_WORKED_COMMIT="3ed4c2df" # including BIG improvements in MPPI !! must have !!!
 
 echo "ROSDISTRO=$ROSDISTRO"
 echo "WORKSPACE=$WORKSPACE"
@@ -29,36 +29,49 @@ if [[ $exit_code -ne 0 ]]; then
   exit
 fi
 
+# In order to build the latest mppi_controllers, we need to build navigation2 from source.
+# when comping from source, trying out several commit, below is the latest results
+#
+# ===== mppi_controllers build procedures =====
+# ref: https://github.com/ros-planning/navigation2/issues/3898#issuecomment-1776063536
+#   0. Ensure the navigation2 binary are all in newest version. ie. 1.1.12
+#   1. remove the nav2_mppi_controller binary package (ex: ros-humble-nav2-mppi-controller)
+#      ros-humble-nav2-bringup will be removed as well
+#   2. copy the nav2_mppi_controller source from navigation2 repo into workspace
+#   3. colcon build the nav2_mppi_controller package
+#   4. re-install ros-humble-nav2-bringup
+#
+# 3ed4c2 worked (11/17)
+#   we should be able build any commit from now on following above procedures
+# humble (57f55c) failed (10/16)
+# 3b791e failed (10/13)
+# 1b97e3 failed [MPPI Optimization] (10/10)
+# 1.1.12 ok (10/4)
+# 1.1.11 failed
+# 1.1.10 ok
+# 1.1.9 ok (8/4)
+
+# ref: MPPI crashing on loading plug-ings #3767
+# https://github.com/ros-planning/navigation2/issues/3767
+
 if [[ $ROSDISTRO == "humble" ]]; then
   echo "Install mppi_controllers from source."
-  # In order to build the latest mppi_controllers, we need to build navigation2 from source.
   #
-  # 1. remove the mppi_controllers binary package
+  echo "1. remove the mppi_controllers binary package"
   sudo apt remove ros-$ROSDISTRO-nav2-mppi-controller -y
   #
-  # when comping from source, trying out several commit, below is the latest results
   #
-  # humble (57f55c) failed (10/16)
-  # 3b791e failed (10/13)
-  # 1b97e3 failed [MPPI Optimization] (10/10)
-  # 1.1.12 ok (10/4)
-  # 1.1.11 failed
-  # 1.1.10 ok
-  # 1.1.9 ok (8/4)
-
-  # ref: MPPI crashing on loading plug-ings #3767
-  # https://github.com/ros-planning/navigation2/issues/3767
-  #
+  echo "2. copy the nav2_mppi_controller source from navigation2 repo into workspace"
   rm -rf /tmp/navigation2 > /dev/null 2>&1
   git clone https://github.com/ros-planning/navigation2 /tmp/navigation2 -b "$ROSDISTRO"
   cd /tmp/navigation2 && git checkout $LATEST_WORKED_COMMIT
   cp -R /tmp/navigation2/nav2_mppi_controller "$HOME/$WORKSPACE/src"
 
-  # 2. build
+  echo "3. colcon build the nav2_mppi_controller package"
   cd "$HOME/$WORKSPACE"
   rm -rf build/nav2_mppi_controller install/nav2_mppi_controller > /dev/null 2>&1
   colcon build --symlink-install --packages-select nav2_mppi_controller
   #
-  # 3. re-install nav2-bringup
+  echo "4. re-install ros-humble-nav2-bringup"
   sudo apt install ros-$ROSDISTRO-nav2-bringup -y
 fi
