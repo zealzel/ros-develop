d8771dc4b209b2d9d81616cb51e9b3924ccf3d69
zealzel <zealzel@gmail.com>
2024-03-04 14:23:04 +0800

    Update set_network.sh

--- setup_raspberry/zbot_lino/set_network.sh
+++ setup_raspberry/zbot_lino/set_network.sh
@@ -2,11 +2,14 @@
 script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source "$script_dir/../../scripts/utils.sh"
 
+# get env BASE
+BASE=${LINOROBOT2_BASE-"zbotlino2"}
+
 # copy netplan config file
 filepath="/etc/netplan/50-cloud-init.yaml"
 sudo cp "$filepath" "$filepath.bak"
-sudo cp 50-cloud-init.yaml "$filepath"
+sudo cp "50-cloud-init-$BASE.yaml" "$filepath"
 
 # copy cyclonedds config file
-cp cyclonedds.xml $HOME
+cp "cyclonedds-$BASE.xml" $HOME/cyclonedds.xml
 append_bashrc 'export CYCLONEDDS_URI=$HOME/cyclonedds.xml'
