7b79cfe19cb3827eccc10b34bf19fc5680cc592a
zealzel <zealzel@gmail.com>
2024-03-04 17:43:06 +0800

    Add overclock.sh

---
+++ setup_raspberry/zbot_lino/overclock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# modify /boot/firmware/config.txt, add below lines
+# arm_freq=2100
+# gpu_freq=750
+# over_voltage=6
+# force_turbo=1
+#
+# copy boot-firmware-config.txt to /boot/firmware/config.txt
+sudo cp boot-firmware-config.txt /boot/firmware/config.txt
