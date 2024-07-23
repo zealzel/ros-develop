ab5fd67a09ce41a24e1fcbbf7a3dca754c66817c
zealzel <zealzel@gmail.com>
2024-07-23 17:47:28 +0800

    update turtlebot3_src.sh

--- setup_vm/simulations_turtlebot3/turtlebot3_src.sh
+++ setup_vm/simulations_turtlebot3/turtlebot3_src.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+echo "setup turtlebot3 simulation"
 script_dir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-install_from_source_sh="$(realpath "$script_dir"/../../scripts/install_from_source.sh)"
+install_from_source_sh="$(realpath $script_dir/../../scripts/install_from_source.sh)"
+source "$script_dir/../../scripts/argparse_ros.sh"
+parse_args "$@"
+WORKSPACE=${parsed_args["workspace"]-simulations}
+[ "$VERBOSE" == true ] && print_args
 
-ROS_DISTRO="${ROS_DISTRO-humble}"
-WORKSPACE="${1-simulations}"
+source /opt/ros/${ROSDISTRO}/setup.bash >/dev/null 2>&1 || exit_code=$?
+if [[ $exit_code -ne 0 ]]; then
+  echo "/opt/ros/$ROSDISTRO/setup.sh does not exist."
+  print_usage
+  exit
+fi
 
 echo
 echo ====================================================================
